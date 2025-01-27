dda2d6396d3924ec75227e22cf5d22bd6ae53e08
Andres Castro <wilmerandres25@gmail.com>
2025-01-27 16:24:07 -0400

    Add the feature to log user's request in the console
    The exercises 3.7 and 3.8 of fullstackopen part 3, section A
    have been completed.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -38,6 +39,12 @@ const generateId = () => {
     if (!unique) return ranId
 
 }
+
+morgan.token('payload', (request) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :payload'))
 
 app.get('/', (request, response) => {
 
